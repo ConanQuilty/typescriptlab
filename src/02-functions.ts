5ba5952faae68368d416f22c38d9d61ff39b11ec
ConanQuilty <20107701@setu.ie>
2026-09-21 15:09:14 +0100

    Add addInterest to add an interest for a singular friend

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -60,3 +60,12 @@ function findFriends(
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
 
+function addInterest(friend: Friend,interest:string) {
+    if (friend.interests !== undefined){
+        friend.interests.push(interest);
+    } else {
+        friend.interests = [interest];
+    }
+    return friend.interests
+}
+console.log(addInterest(friends[0], 'Politics'))
